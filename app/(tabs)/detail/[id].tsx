21ff0b98a278f08e0ab114f118c1d142532b3607
yu-matsumura <29217415+yuma1217@users.noreply.github.com>
2024-02-21 18:27:34 +0900

    複雑なLayoutを足す

---
+++ app/(tabs)/detail/[id].tsx
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+export default function Page() {
+  return (
+    <View>
+      <Text>Page</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
