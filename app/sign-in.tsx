d953a777ce6ce54c4cbd20fcc339ff49cf5bf5ac
yu-matsumura <29217415+yuma1217@users.noreply.github.com>
2024-04-10 15:09:44 +0900

    ログインページ

--- app/sign-in.tsx
+++ app/sign-in.tsx
@@ -1,32 +1,57 @@
-import { View, Text } from "react-native";
-import React from "react";
-import { Button, TextInput } from "react-native-paper";
+import { View, Text, StyleSheet } from "react-native";
+import React, { useState } from "react";
+import { Button, Card, TextInput } from "react-native-paper";
 import { useSession } from "@/components/ctx";
 import { router } from "expo-router";
 
 const SignInPage = () => {
   const { signIn } = useSession();
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = () => {
+    // ログインロジック
+    console.log(username, password);
+  };
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <TextInput label="Username" placeholder="ログイン名" />
-      <TextInput label="Password" placeholder="パスワード" />
-      <Button
-        mode="contained"
-        onPress={() => {
-          signIn();
-          router.replace("/(app)");
-        }}
-      >
-        SignIn
-      </Button>
+    <View style={styles.container}>
+      <Card style={styles.card}>
+        <Card.Title title="ログインページ" />
+        <Card.Content>
+          <TextInput
+            label="Username"
+            value={username}
+            onChangeText={(text) => setUsername(text)}
+            mode="outlined"
+          />
+          <TextInput
+            label="Password"
+            value={password}
+            onChangeText={(text) => setPassword(text)}
+            secureTextEntry
+            mode="outlined"
+            style={styles.textInput}
+          />
+          <Button mode="contained" onPress={handleLogin}>
+            ログイン
+          </Button>
+        </Card.Content>
+      </Card>
     </View>
   );
 };
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    padding: 20,
+  },
+  card: {
+    padding: 16,
+  },
+  textInput: {
+    marginBottom: 16,
+  },
+});
 export default SignInPage;
