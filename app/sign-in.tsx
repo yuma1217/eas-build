03fb028494c9c7474aabe57aff0c481ae47d451d
yu-matsumura <29217415+yuma1217@users.noreply.github.com>
2024-05-27 16:33:57 +0900

    Feature/secure store (#1)
    * expo update
    
    * expo secure store
    
    * ログインページ
    
    * ログインページ
    
    * wip
    
    * SucureStore

--- app/sign-in.tsx
+++ app/sign-in.tsx
@@ -1,30 +1,67 @@
-import { View, Text } from "react-native";
-import React from "react";
-import { Button } from "react-native-paper";
+import { View, Text, StyleSheet } from "react-native";
+import React, { useState } from "react";
+import { Button, Card, TextInput } from "react-native-paper";
 import { useSession } from "@/components/ctx";
 import { router } from "expo-router";
+import { SecureStorage } from "@/context/secure-storage";
 
 const SignInPage = () => {
   const { signIn } = useSession();
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = async () => {
+    // ログインロジック
+    console.log(username, password);
+    await SecureStorage.save("username", username);
+  };
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Button
-        mode="contained"
-        onPress={() => {
-          signIn();
-          router.replace("/(app)");
-        }}
-      >
-        SignIn
-      </Button>
+    <View style={styles.container}>
+      <Card style={styles.card}>
+        <Card.Title title="ログインページ" />
+        <Card.Content>
+          <TextInput
+            label="Username"
+            value={username}
+            onChangeText={(text) => setUsername(text)}
+            mode="outlined"
+          />
+          <TextInput
+            label="Password"
+            value={password}
+            onChangeText={(text) => setPassword(text)}
+            secureTextEntry
+            mode="outlined"
+            style={styles.textInput}
+          />
+          <Button mode="contained" onPress={handleLogin}>
+            ログイン
+          </Button>
+          <Button
+            mode="contained"
+            onPress={async () => {
+              await SecureStorage.getValueFor("username");
+            }}
+          >
+            確認
+          </Button>
+        </Card.Content>
+      </Card>
     </View>
   );
 };
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    padding: 20,
+  },
+  card: {
+    padding: 16,
+  },
+  textInput: {
+    marginBottom: 16,
+  },
+});
 export default SignInPage;
